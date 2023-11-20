c6aac5f84e84cb0cf2c3b531cd47cfc268c9cba0
Chiputao <420958094@qq.com>
2023-11-20 20:24:45 +0000

    fix format

--- src/main/java/edu/northeastern/cs5500/starterbot/command/SpawnCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/SpawnCommand.java
@@ -57,7 +57,9 @@ public class SpawnCommand implements SlashCommandHandler, ButtonHandler {
 
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setTitle(String.format("A wild %s appear!", species.getName()));
-        embedBuilder.setDescription(String.format("It costs 5 coins to catch %s. What will you do?", species.getName()));
+        embedBuilder.setDescription(
+                String.format(
+                        "It costs 5 coins to catch %s. What will you do?", species.getName()));
         embedBuilder.addField("Level", Integer.toString(pokemon.getLevel()), false);
         embedBuilder.setThumbnail(species.getImageUrl());
 
