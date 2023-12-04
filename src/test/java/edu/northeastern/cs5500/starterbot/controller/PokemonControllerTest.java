44eeb0ff18c84b6d11603bc3badc0a1c8391ef7f
Chiputao <yang.huir@northeastern.edu>
2023-12-04 22:36:55 +0000

    test PokemonEvolutionController

--- src/test/java/edu/northeastern/cs5500/starterbot/controller/PokemonControllerTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/controller/PokemonControllerTest.java
@@ -5,7 +5,6 @@ import static com.google.common.truth.Truth.assertThat;
 import edu.northeastern.cs5500.starterbot.model.Pokemon;
 import edu.northeastern.cs5500.starterbot.repository.InMemoryRepository;
 import edu.northeastern.cs5500.starterbot.service.PokemonDataService;
-import edu.northeastern.cs5500.starterbot.service.PokemonEvolutionService;
 import org.junit.jupiter.api.Test;
 
 public class PokemonControllerTest {
@@ -16,15 +15,10 @@ public class PokemonControllerTest {
                 new PokemonDataService(
                         "src/test/java/edu/northeastern/cs5500/starterbot/resources/pokeDataTest_2.json");
         PokedexController pokedexController = new PokedexController(pokemonDataService);
-        PokemonEvolutionService pokemonEvolutionService =
-                new PokemonEvolutionService(
-                        "src/test/java/edu/northeastern/cs5500/starterbot/resources/evolution-chainTest.json");
         PokemonController pokemonController =
                 new PokemonController(new InMemoryRepository<>(), pokemonDataService);
 
-        pokemonController.pokemonEvolutionService = pokemonEvolutionService;
         pokemonController.pokedexController = pokedexController;
-        pokemonController.pokemonEvolutionList = pokemonEvolutionService.getPokemonEvolutionList();
         pokedexController.pokemonDataList = pokemonDataService.getPokemonDataList();
 
         return pokemonController;
@@ -66,30 +60,5 @@ public class PokemonControllerTest {
         // Attack             : 🛡️ Phys. 111  | 🔮 Sp. 11
         // Defense            : 🛡️ Phys. 10   | 🛡️ Sp. 11
         // System.out.println(attack + defense);
-    }
-
-    @Test
-    public void testIsEvolved() {
-        Pokemon pokemon =
-                Pokemon.builder()
-                        .pokedexNumber(1)
-                        .level(5)
-                        .currentHp(45)
-                        .hp(45)
-                        .attack(49)
-                        .defense(49)
-                        .specialAttack(65)
-                        .specialDefense(65)
-                        .speed(45)
-                        .build();
-
-        PokemonController pokemonController = getPokemonController();
-        pokemonController.pokemonRepository.add(pokemon);
-        boolean isEvolved = pokemonController.evolvePokemon(pokemon.getId().toString());
-        assertThat(isEvolved).isTrue();
-        assertThat(pokemon.getEvolvedFrom()).isEqualTo("Bulbasaur");
-
-        // should evolve to Ivysaur
-        assertThat(pokemon.getPokedexNumber()).isEqualTo(2);
     }
 }
