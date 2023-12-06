f1fc37f15c33d7482fb614fddd7d0b9d38d38cad
ycChu711 <129474865+ycChu711@users.noreply.github.com>
2023-12-06 08:29:29 +0000

    fix formating issue

--- src/main/java/edu/northeastern/cs5500/starterbot/command/HelpCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/HelpCommand.java
@@ -34,18 +34,18 @@ public class HelpCommand implements SlashCommandHandler {
         log.info("event: /help");
 
         event.reply(
-                String.format(
-                        "```🌟🌟🌟Available Pokebot Commands 🌟🌟🌟\n\n"
-                                + "🌈👾 Pokemon 👾🌈\n"
-                                + "🐣 /spawn: Spawn a random Pokemon for the user to try to catch\n"
-                                + "🦄 /pokemon: View your pokemon status\n"
-                                + "🔢 /my: Get your pokemon's stats by typing its number\n"
-                                + "⚔️ /battle: Start a battle with a random Pokémon, with a cost of only 5 coins\n"
-                                + "🍹 /feed: Choose the Pokemon you want to feed by typing its number\n\n"
-                                + "💫🧙‍♂️ Trainer 🧙‍♂️💫\n"
-                                + "💎 /daily: Reward the player with random coins upon completing their daily check-in\n"
-                                + "🛍️ /shop: A shop for trainer to purchase food\n"
-                                + "📊 /status: View your current status```"))
+                        String.format(
+                                "```🌟🌟🌟Available Pokebot Commands 🌟🌟🌟\n\n"
+                                        + "🌈👾 Pokemon 👾🌈\n"
+                                        + "🐣 /spawn: Spawn a random Pokemon for the user to try to catch\n"
+                                        + "🦄 /pokemon: View your pokemon status\n"
+                                        + "🔢 /my: Get your pokemon's stats by typing its number\n"
+                                        + "⚔️ /battle: Start a battle with a random Pokémon, with a cost of only 5 coins\n"
+                                        + "🍹 /feed: Choose the Pokemon you want to feed by typing its number\n\n"
+                                        + "💫🧙‍♂️ Trainer 🧙‍♂️💫\n"
+                                        + "💎 /daily: Reward the player with random coins upon completing their daily check-in\n"
+                                        + "🛍️ /shop: A shop for trainer to purchase food\n"
+                                        + "📊 /status: View your current status```"))
                 .queue();
     }
 }
