988bf9017351a8340a41845b1f029bfe997b519f
z-q-ying <116849653+z-q-ying@users.noreply.github.com>
2023-12-10 22:31:40 +0000

    Finetune colors

--- src/main/java/edu/northeastern/cs5500/starterbot/model/PokemonSpecies.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/PokemonSpecies.java
@@ -1,6 +1,5 @@
 package edu.northeastern.cs5500.starterbot.model;
 
-import java.awt.Color;
 import java.util.Random;
 import javax.annotation.Nonnull;
 import lombok.Builder;
@@ -27,27 +26,7 @@ public class PokemonSpecies { // Not implementing Model
 
     /** Return the representitive color for a pokemon species. */
     public Integer getSpeciesColor() {
-        if (types.length == 1) {
-            return types[0].getColor();
-        }
-        int totalRed = 0;
-        int totalGreen = 0;
-        int totalBlue = 0;
-        int count = types.length;
-
-        for (int i = 0; i < count; i++) {
-            Color color = new Color(types[i].getColor());
-            totalRed += color.getRed();
-            totalGreen += color.getGreen();
-            totalBlue += color.getBlue();
-        }
-
-        int red = totalRed / count;
-        int green = totalGreen / count;
-        int blue = totalBlue / count;
-
-        Color newColor = new Color(red, green, blue);
-        return newColor.getRed();
+        return types[0].getColor();
     }
 
     // Sample msg:
